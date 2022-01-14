a6766a3f0e6f4f140d45e0f734b76a9d655361c0
fabian pacheco <fabian@gmail.com>
2022-01-14 17:24:44 -0300

    conectar checkbox con eel

--- web/main.js
+++ web/main.js
@@ -1,22 +1,22 @@
 
-async function put_path(){
+async function put_path() {
     const path = await eel.get_path()();
     document.getElementById('path').innerHTML = path;
 }
 put_path();
 
-async function change_directory(){
+async function change_directory() {
     const path = await eel.change_directory()();
     document.getElementById('path').innerHTML = path;
 }
 
-async function download_js(){
+async function download_js() {
     const mensaje = await eel.download()();
     document.getElementById('error').innerHTML = mensaje;
 }
 
 eel.expose(get_path);
-function get_path(){
+function get_path() {
     const elemento = document.getElementById("path");
     const data = elemento.innerHTML;
     console.log(data)
@@ -24,9 +24,21 @@ function get_path(){
 }
 
 
-eel.expose(get_url);
-function get_url(){
+eel.expose(get_data);
+function get_data() {
     const elemento = document.getElementById("url");
-    const data = elemento.value;
+    const ext = document.getElementsByName("ext");
+    const mp3 = ext[0];
+    const mp4 = ext[1];
+    var data_ext;
+
+    if (mp3.checked && mp4.checked) {
+        console.log("error");
+    } else if (mp3.checked) {
+        data_ext = mp3.id;
+    } else if (mp4.checked) {
+        data_ext = mp4.id;
+    }
+    const data = { "url": elemento.value, "ext": data_ext };
     return data;
 }
